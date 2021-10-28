c94f06e7086081e1969507b75ec4b1597817f15c
Sergio Baumgartner <66957567+brunobaumgartner@users.noreply.github.com>
2021-10-28 10:46:57 -0300

    Primeiro push

---
+++ src/server.js
@@ -0,0 +1,46 @@
+const PORTA = 5000;
+const express = require('express')
+const bancoDeDados = require("./bancoDeDados")
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.get('/produtos', (req, res, next) => {
+    res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.deleteProduto(req.params.id)
+    res.send(produto)
+})
+
+app.listen(PORTA, () => {
+    console.log(`Servidor executando na porta ${PORTA}.`)
+})
